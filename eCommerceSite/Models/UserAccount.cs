f10bb0b57ee83f6d7e8e1e2405d766d1460d33a6
Kramer Jensen <Kramerjensend@gmail.com>
2021-02-13 17:29:27 -0800

    Added a register page and more validation

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -23,19 +23,24 @@ namespace eCommerceSite.Models
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
+        [StringLength(200)]
         public string Email { get; set; }
 
         [Compare(nameof(Email))] // nameof for refactoring purposes, strings don't have refactor support
         [Required]
+        [Display(Name = "Confirm Email")]
         public string ConfirmEmail { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(1000, MinimumLength = 8, ErrorMessage = "Password must be at least {2} characters!")]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
         [DataType(DataType.Date)] // Time is ignored.
