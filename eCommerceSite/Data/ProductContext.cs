18d0fe2002dea3767ec5903cdfec86157d5b569b
Kramer Jensen <Kramerjensend@gmail.com>
2021-02-13 16:43:36 -0800

    Added UserAccount to DB using code first migration

--- eCommerceSite/Data/ProductContext.cs
+++ eCommerceSite/Data/ProductContext.cs
@@ -10,10 +10,10 @@ namespace eCommerceSite.Data
 {
     public class ProductContext : DbContext
     {
-        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
-        {
-        }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) {}
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
     }
 }
